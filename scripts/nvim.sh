f7677decfc16fc40f508fff1f8e07615b18df3d5
fission1110 <rysnxedge1110@gmail.com>
2022-10-12 01:34:57 +0000

    link nvim to vim so you can sudo vim easier

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,4 +1,5 @@
 wget -O /neovim.deb "${V_NVIM_URL}" \
     && dpkg -i /neovim.deb \
     && rm /neovim.deb \
-    && rm -r /lib/nvim
+    && rm -r /lib/nvim \
+    && ln -s /usr/bin/nvim /usr/local/bin/vim
