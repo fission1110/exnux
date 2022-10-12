550693e3fb1390c8a962aa09672c5f1dc71dfdf5
fission1110 <rysnxedge1110@gmail.com>
2022-10-12 02:04:59 +0000

    Link gofmt to the path

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -2,4 +2,5 @@ mkdir -p /usr/local/src/golang \
     && wget -O /usr/local/src/golang/go.tar.gz "${V_GOLANG_URL}" \
     && tar -I pigz -C /usr/local -xf /usr/local/src/golang/go.tar.gz \
     && ln -s /usr/local/go/bin/go /usr/local/bin/go \
+    && ln -s /usr/local/go/bin/gofmt /usr/local/bin/gofmt \
     && rm -r /usr/local/src/golang
