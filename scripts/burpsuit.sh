54a8ffeab28769d4e47787622b2ba1729021dceb
fission1110 <rysnxedge1110@gmail.com>
2022-10-17 04:04:30 +0000

    Add burp-proxy and burp-noproxy commands that globally set the http_proxy environment variable in tmux so we can pipe tools through burp again

--- scripts/burpsuit.sh
+++ scripts/burpsuit.sh
@@ -1,4 +1,8 @@
 mkdir -p /usr/local/src/burp/ \
     && wget -O /usr/local/src/burp/burp.jar "${V_BURPSUIT_URL}" \
     && echo "java -Xms2G -Xmx5G -jar /usr/local/src/burp/burp.jar" > /usr/local/bin/burp \
-    && chmod +x /usr/local/bin/burp
+    && chmod +x /usr/local/bin/burp \
+    && echo "#!/bin/bash\ntmux setenv http_proxy 'http://localhost:8080/'" > /usr/local/bin/burp-proxy \
+    && echo "#!/bin/bash\ntmux setenv http_proxy ''" > /usr/local/bin/burp-noproxy \
+    && chmod +x /usr/local/bin/burp-proxy \
+    && chmod +x /usr/local/bin/burp-noproxy
