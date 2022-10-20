b40c1ce6519bb58016170c39333755ff421796e8
fission1110 <rysnxedge1110@gmail.com>
2022-10-20 21:18:25 +0000

    Reduce the max tokens to make output smaller

--- dotfiles/tools/openai_vim.py
+++ dotfiles/tools/openai_vim.py
@@ -4,10 +4,7 @@ import openai
 import sys
 
 # read text from stdin
-prompt = sys.stdin.read()
-
-# strip trailing newline
-prompt = prompt.strip()
+prompt = sys.stdin.read().strip()
 
 # read api key from file ~/.config/openai/api_key
 with open(os.path.expanduser("~/.config/openai/api_key")) as f:
@@ -21,7 +18,7 @@ response = openai.Completion.create(
   model="text-davinci-002",
   prompt=prompt,
   temperature=0.9,
-  max_tokens=250,
+  max_tokens=100,
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0
