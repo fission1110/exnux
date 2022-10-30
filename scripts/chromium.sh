7ef718134d573258a75d9afa2e447b28061b51a1
fission1110 <rysnxedge1110@gmail.com>
2022-10-30 18:24:06 +0000

    Set chromium as the default browser and lower the priority of the system76 ppa so it doesn't mess with too many things

--- scripts/chromium.sh
+++ scripts/chromium.sh
@@ -1,2 +1,9 @@
 add-apt-repository ppa:system76/pop
 apt-get install -y chromium libavcodec-extra
+update-alternatives --config x-www-browser /usr/bin/chromium
+update-alternatives --config gnome-www-browser /usr/bin/chromium
+xdg-settings set default-web-browser chromium.desktop
+
+echo "Package: *
+Pin: release o=LP-PPA-system76-pop
+Pin-Priority: 400" > /etc/apt/preferences.d/system76-pop
