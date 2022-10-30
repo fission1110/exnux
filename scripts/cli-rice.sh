16f4dbe71877df8dcf5b054113dde69337663276
fission1110 <rysnxedge1110@gmail.com>
2022-10-30 17:59:39 +0000

    Add btop with btop.conf settings

--- scripts/cli-rice.sh
+++ scripts/cli-rice.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 sudo -E -u $USERNAME -s "PATH=$PATH" "HOME=/home/$USERNAME" cargo install exa bat procs tokei
+
+# btop++
+mkdir -p /usr/local/src/btop
+wget -O /usr/local/src/btop/btop-musl.tbz https://github.com/aristocratos/btop/releases/download/v1.2.12/btop-x86_64-linux-musl.tbz
+tar -xvf /usr/local/src/btop/btop-musl.tbz -C /usr/local/src/btop
+rm /usr/local/src/btop/btop-musl.tbz
+cd /usr/local/src/btop
+/usr/local/src/btop/install.sh
