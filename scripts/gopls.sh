111ca3d6e6bc86b5013c9fbc1939835e08e1f89b
fission1110 <rysnxedge1110@gmail.com>
2022-11-01 02:45:15 +0000

    Because of re-order, /home/nonroot/go exists

--- scripts/gopls.sh
+++ scripts/gopls.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-mkdir /home/$USERNAME/go \
-    && chown $USERNAME:$USERNAME /home/$USERNAME/go \
-    && sudo -E -u $USERNAME -s "PATH=$PATH" "HOME=/home/$USERNAME" go install golang.org/x/tools/gopls@latest \
+sudo -E -u $USERNAME -s "PATH=$PATH" "HOME=/home/$USERNAME" go install golang.org/x/tools/gopls@latest \
     && sudo -E -u $USERNAME -s "PATH=$PATH" "HOME=/home/$USERNAME" go clean --cache
