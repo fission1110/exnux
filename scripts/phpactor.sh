6026c46a8722f49009edfb2b34dd48f6679f6a9a
fission1110 <rysnxedge1110@gmail.com>
2023-03-05 19:47:43 +0000

    Lock phpactor to version 2022.11.12 because later versions require PHP 8.0 which isn't in the ubuntu repositories yet.

--- scripts/phpactor.sh
+++ scripts/phpactor.sh
@@ -9,7 +9,7 @@ cd /usr/local/bin \
 # Phpactor
 mkdir /usr/local/src/phpactor \
     && chown $USERNAME:$USERNAME /usr/local/src/phpactor \
-    && sudo -E -u $USERNAME -s "PATH=$PATH" "HOME=/home/$USERNAME" git clone --depth 1 https://github.com/phpactor/phpactor /usr/local/src/phpactor \
+    && sudo -E -u $USERNAME -s "PATH=$PATH" "HOME=/home/$USERNAME" git clone --depth 1 -b '2022.11.12' https://github.com/phpactor/phpactor /usr/local/src/phpactor \
     && cd /usr/local/src/phpactor \
     && sudo -E -u $USERNAME -s "PATH=$PATH" "HOME=/home/$USERNAME" composer update \
     && sudo -E -u $USERNAME -s "PATH=$PATH" "HOME=/home/$USERNAME" composer install
