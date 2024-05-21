f897db8a49d0783373a4be34ad1eea3c011bf6e3
fission1110 <rysnxedge1110@gmail.com>
2024-05-21 03:32:03 +0000

    r2pm No longer requires init or update
    Fix r2ghidra

--- scripts/radare2.sh
+++ scripts/radare2.sh
@@ -8,8 +8,6 @@ mkdir -p /usr/local/src/radare2 \
     && dpkg -i /usr/local/src/radare2/radare2-dev.deb \
     && rm -rf /usr/local/src/radare2 \
     && update-alternatives --install /usr/bin/python python /usr/bin/python3 20 \
-    && sudo -E -u $USERNAME "HOME=/home/$USERNAME" r2pm init \
-    && sudo -E -u $USERNAME "HOME=/home/$USERNAME" r2pm update \
     && sudo -E -u $USERNAME "HOME=/home/$USERNAME" r2pm -i r2ghidra
 `#    && r2pm -gi r2dec` \
     #&& sudo -E -u $USERNAME "HOME=/home/$USERNAME" r2pm -i r2frida
