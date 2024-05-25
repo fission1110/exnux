fcab67523fab70c10f66d3f63f74f2cf472907f6
fission1110 <rysnxedge1110@gmail.com>
2024-05-25 02:20:31 +0000

    Add dap for c/c++/Rust and PHP

--- scripts/dap.sh
+++ scripts/dap.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Install debugpy for Python
 mkdir -p /usr/local/src/debugpy \
     && python3 -m venv /usr/local/src/debugpy \
     && /usr/local/src/debugpy/bin/pip install --upgrade pip \
     && /usr/local/src/debugpy/bin/pip install debugpy
 
+# Install delve for Go
 sudo -E -u $USERNAME -s "PATH=$PATH" "HOME=/home/$USERNAME" go install github.com/go-delve/delve/cmd/dlv@latest
+
+# Install lldb for C/C++/Rust
+apt-get -y install lldb-9
+
+# Install vscode-php-debug for PHP
+git clone https://github.com/xdebug/vscode-php-debug.git /usr/local/src/vscode-php-debug \
+    && cd /usr/local/src/vscode-php-debug \
+    && npm install && npm run build
