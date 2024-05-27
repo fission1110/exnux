06a2d66bdf21c296b680f35d6de03c68fd658be9
fission1110 <rysnxedge1110@gmail.com>
2024-05-27 03:58:54 +0000

    Write a simple function to add the attached lsp to the lualine

--- dotfiles/.config/nvim/lua/ex_lualine.lua
+++ dotfiles/.config/nvim/lua/ex_lualine.lua
@@ -1,6 +1,26 @@
 -- ---------
 -- lualine theme
 -- ---------
+local function getActiveLsp()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local clients = vim.lsp.get_clients({ bufnr = bufnr })
+	local prefix = " "
+	local client_names = ""
+	if next(clients) == nil then
+		return ""
+	end
+
+	for _, client in pairs(clients) do
+		if client.name ~= "GitHub Copilot" then
+			client_names = client_names .. client.name .. " "
+		end
+	end
+	if client_names == "" then
+		return ""
+	end
+	return prefix .. client_names
+end
+
 require('lualine').setup({
     options = {
         globalstatus = true,
@@ -8,7 +28,7 @@ require('lualine').setup({
     },
     sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_b = {'branch', 'diff', 'diagnostics', getActiveLsp},
         lualine_c = {{'filename', file_status = true, path = 1}},
         lualine_x = {'encoding', 'filetype'},
         lualine_y = {'progress'},
