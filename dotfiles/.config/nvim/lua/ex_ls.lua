ba5892aeafcc1efacaf5b01804342584d95e0f6d
fission1110 <rysnxedge1110@gmail.com>
2024-05-27 03:31:02 +0000

    luasnip can't use S-L/S-H in insert because that's how you make capital letters, doh!

--- dotfiles/.config/nvim/lua/ex_ls.lua
+++ dotfiles/.config/nvim/lua/ex_ls.lua
@@ -4,8 +4,8 @@ local ls = require("luasnip")
 
 require("luasnip.loaders.from_snipmate").lazy_load({paths = {"/home/" .. username .. "/.config/nvim/snippets"}})
 vim.keymap.set({"i"}, "<C-L>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<S-L>", function() ls.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<S-H>", function() ls.jump(-1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-]>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-[>", function() ls.jump(-1) end, {silent = true})
 
 vim.keymap.set({"i", "s"}, "<C-E>", function()
 	if ls.choice_active() then
