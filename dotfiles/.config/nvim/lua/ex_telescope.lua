c45cd5a735526029075a10cfe95bc243522b76d4
fission1110 <rysnxedge1110@gmail.com>
2024-05-30 01:58:03 +0000

    Add zoxide telescope plugin and configure it to work with nerdtree

--- dotfiles/.config/nvim/lua/ex_telescope.lua
+++ dotfiles/.config/nvim/lua/ex_telescope.lua
@@ -20,6 +20,25 @@ telescope.setup {
       limit = 1000,
     },
   },
+  extensions = {
+    zoxide = {
+      mappings = {
+        default = {
+          after_action = function(selection)
+            -- get the window number
+            local winnr = vim.api.nvim_get_current_win()
+            -- CD to the selected directory
+            vim.cmd('NERDTreeCWD')
+            -- Set focus back to the window
+            vim.api.nvim_set_current_win(winnr)
+          end
+        },
+      },
+    },
+  },
 }
 telescope.load_extension('gh')
 telescope.load_extension('dap')
+telescope.load_extension('zoxide')
+
+vim.api.nvim_set_keymap('n', '<leader>z', '<cmd>Telescope zoxide list<cr>', { noremap = true, silent = true })
