b272761883a38d79e70324a16e1164362177a6a2
fission1110 <rysnxedge1110@gmail.com>
2024-06-01 06:13:50 +0000

    Remove depricated omnifunc setting. Don't use anyways. nvim_cmp handles this.

--- dotfiles/.config/nvim/lua/ex_lsp.lua
+++ dotfiles/.config/nvim/lua/ex_lsp.lua
@@ -12,9 +12,6 @@ vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<C
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
